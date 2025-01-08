d8e6bca485e7969f3afeb72a768ee3ea8843a5c2
R.G <rgudchenko@ya.ru>
2025-01-08 21:27:39 +0300

    code2prompt settings

--- code2prompt/default.sh
+++ code2prompt/default.sh
@@ -1,1 +1,1 @@
-code2prompt . --exclude="*.json,*.js,*.exe,*.bin,*.dll,*.map,node_modules/**,data/**,code2prompt/**" --output=./code2prompt/output.txt  --tokens -t code2prompt/templates/fix-bugs.hbs
+code2prompt . --exclude="*.json,*.js,*.exe,*.bin,*.dll,*.map,node_modules/**,data/**,code2prompt/templates/**,*.hbs" --output=./code2prompt/output.txt  --tokens -t code2prompt/templates/fix-bugs.hbs
