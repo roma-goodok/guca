7e37e89b4b2b2c74dabd9fd8445319db1f575499
R.G <rgudchenko@ya.ru>
2025-01-08 17:33:12 +0300

    sh to refactor by code2prompt

---
+++ code2prompt/refactor.sh
@@ -0,0 +1,1 @@
+code2prompt . --exclude="*.json,*.js,*.exe,*.bin,*.dll,*.map,node_modules/**,data/**,code2prompt/**" --output=./code2prompt/output.txt  --tokens -t code2prompt/templates/refactor.hbs
