843df5e70b072cf6f2e55ce0843882343e817b73
Roman Gudchenko <roma.goodok@gmail.com>
2025-12-24 21:20:31 +0300

    moving hole particle example

--- code2prompt/new_features.sh
+++ code2prompt/new_features.sh
@@ -2,7 +2,7 @@
 set -eu
 
 code2prompt \
-  --exclude="*.json,*.js,*.exe,*.bin,*.dll,*.map,*.cs,node_modules/**,data/**,code2prompt/templates/**,*.hbs" \
+  --exclude="*.json,*.js,*.exe,*.bin,*.dll,*.map,*.cs,node_modules/**,data/**,code2prompt/templates/**,scripts/**,src/__tests__/**,*.hbs" \
   --tokens "format" --encoding=cl100k \
   -t ./code2prompt/templates/new-features.hbs \
   . ./code2prompt/output.txt
